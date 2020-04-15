46415361975336584bf7be266390ce923d318ad2
logm <loganmarkle@gmail.com>
2020-04-15 15:21:50 -0700

    added search v1

--- recipefinder/project/app/routes.py
+++ recipefinder/project/app/routes.py
@@ -16,8 +16,28 @@ class RecipeList:
 			self.recipeList.insert(0, r)
 			# self.recipeList.append(r)
 	recipeList = []
-	def getFilteredList(self):
+	def getFilteredList(self, i_list):
+		if not i_list:
+			return self.recipeList
+		foundList = []
+		notFoundList = []
+		for ing in i_list:
+			print("ing", ing)
+			for r in self.recipeList:
+				print("r", r)
+				for ri in r.ingredient_list:
+					print("ri", ri)
+					if ri == ing:
+						print("found")
+						r.numOfInputIng = r.numOfInputIng + 1
+						foundList.append(r)
+						break
+					else:
+						print("not found")
+						foundList.append(r)
+		self.recipeList = foundList + notFoundList
 		return self.recipeList
+
 
 class RecipeObject:
 	def __init__(self, title_, description_, ingredient_list_, image_url_, link_):
@@ -26,13 +46,14 @@ class RecipeObject:
 		self.ingredient_list = ingredient_list_
 		self.image_url = image_url_
 		self.link = link_
+		self.numOfInputIng = 0
 
 input_ingredient_list = []
 rlist = []
 print("getting recipes from db")
 # recipeTable = db.search(where('type') == 'recipe')
 rq = RecipeList()
-rlist  = rq.getFilteredList()
+rlist  = rq.getFilteredList(None)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -44,6 +65,9 @@ def index():
 	form.inputingredient.data = ""
 
 	print("list of input ingredients", str(input_ingredient_list))
+
+	print("creating new rlist")
+	rlist  = rq.getFilteredList(input_ingredient_list)
 
 
 	return render_template('index.html', title='Home',  recipeList = rlist, form = form, input_ingredient_list = input_ingredient_list)
